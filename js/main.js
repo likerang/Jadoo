ebe73ddb92f6de4ac7fc6b23ce2e3528d56c0c72
likerang <arang9314@naver.com>
2025-05-02 11:15:23 +0900

    testimonial script 완성

--- js/main.js
+++ js/main.js
@@ -5,6 +5,13 @@ const book_a_trip_height = book_a_trip.offsetHeight;
 const ongoing_percent = document.querySelector(".ongoing .percent");
 const ongoing_bar = document.querySelector(".ongoing .bar");
 
+const testimonialList = document.querySelectorAll(".testimonials ul li");
+const testimonial_pager = document.querySelectorAll(".testimonials .pager a");
+const testimonial_upBtn = document.querySelector(".pagination .up"); 
+const testimonial_downBtn = document.querySelector(".pagination .down"); 
+let testimonialIndex = 0;
+
+
 window.addEventListener("scroll", function () {
   if (window.scrollY - 300 > book_a_trip_OST - book_a_trip_height) {
     if(book_a_trip.classList.contains("active") == false) {
@@ -26,3 +33,46 @@ function startNumberAnimation() {
     }
   }, 100);
 }
+
+
+
+testimonial_pager.forEach((pager, index) => {
+  pager.addEventListener(("click"),function(e){
+    e.preventDefault();
+    changeTestimonial(index);
+  });
+});
+
+testimonial_downBtn.addEventListener("click", function(e){
+  if (testimonialIndex < testimonialList.length - 1){
+    testimonialIndex++;
+  }else{
+    testimonialIndex = 0;
+  }
+  changeTestimonial(testimonialIndex);
+  });
+
+  testimonial_upBtn.addEventListener('click', ()=>{
+    if(testimonialIndex>0){
+      testimonialIndex--;
+    }else{
+      testimonialIndex = testimonialList.length - 1;
+    }
+    changeTestimonial(testimonialIndex);
+  })
+
+  function changeTestimonial (idx){
+        //모든 페이저에서 active 클래스 제거
+        testimonial_pager.forEach((item) => {
+          item.classList.remove("active");
+        });
+        
+        //모든 페이저에서 active 클래스 추가
+        testimonial_pager[idx].classList.add("active");
+    
+        testimonialList.forEach((item) => {
+        item.classList.remove("active");
+      });
+      testimonialList[idx].classList.add("active");//현재 번째에 추가
+      testimonialIndex = [idx];
+  }
